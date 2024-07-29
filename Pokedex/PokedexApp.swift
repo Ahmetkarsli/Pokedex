9f4245b97752800413f0f70cbcd6efd5b0d70aa0
ahmetkarsli <ahmet.karsli@icloud.com>
2024-07-30 01:10:25 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ahmet Karsli on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
